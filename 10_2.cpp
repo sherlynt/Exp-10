9607cebaee00444d974b360e62c4644625578681
sherlynt <tiwarisherlyn@gmail.com>
2024-08-28 00:12:30 +0530

    Create 10_2.cpp

---
+++ 10_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream> 
+using namespace std; 
+void swap(int *x, int *y) 
+{
+    int *swap;
+    swap=x;
+    x=y;
+    y=swap;
+}
+
+int main() 
+{
+    int a=3,b=1;
+    swap(a,b);
+    cout<<"Value of a is: "<<a<<"\n";
+    cout<<"Value of b is: "<<b<<"\n";
+    return 0;
+} 
